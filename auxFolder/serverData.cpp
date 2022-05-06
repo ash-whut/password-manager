8c545cf75ef064c4788163d18273e179ea174430
Ashwin Ranjith <ashw.men.in@gmail.com>
2022-05-06 09:30:02 -0500

    Forgot to commit changes in cpp

--- auxFolder/serverData.cpp
+++ auxFolder/serverData.cpp
@@ -1,29 +1,16 @@
 #include "serverData.h"
+#include "Creds.h"
 #include <string>
 #include <vector>
 using namespace std;
 
 void Data::addData(string username, string password){
-    this->userNames.push_back(username);
-    this->passWords.push_back(password);
+    Creds studentData = Creds();
+
+    studentData.setUserName(username);
+    studentData.setPassword(password);
+
+    studentCredentials.push_back(studentData);
+
     return;
-}
-
-string Data::findUserName(string username){
-    bool status = false;
-
-    for (int i = 0; i < this->userNames.size(); ++i){
-        if (username == this->userNames.at(i)){
-            status = true;
-            return this->passWords.at(i);
-        }
-    }
-
-    if (status == false){
-        return "None";
-    }
-}
-
-int Data::existingFileSize(vector<string> uNameVector){
-    return uNameVector.size();
 }
